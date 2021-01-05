09edd7185ea59d1efbd555c55fe9925d67ef9435
Milan Vervaeke <milan.vervaeke@student.howest.be>
2021-01-05 17:18:17 +0100

    ENH

--- gamemode1/app2.js
+++ gamemode1/app2.js
@@ -1,6 +1,7 @@
 //variablen definieren
-var duck, duckhitbox, target, targetdetection, targetdetection2, targetdetection3, targetdetection4, targetdetection5, progressbarbackside, progressbar;
-var myScore, score, testscore, endText, timerAan, mytimer, time, time2;
+var duck, duckHitbox, target, targetDetection, targetDetection2, targetDetection3, targetDetection4, targetDetection5, progressbarBackground, progressbarHealth;
+var lblScore, score, checkScore, endOfGameMessage, timerOn, lblSecondsPast, frames, secondsPast;
+var canShoot;
 //img ophalen
 var link1 = document.createElement('link');
 var link2 = document.createElement('link');
@@ -9,27 +10,27 @@ link1.href = "quak.png";
 link2.href = "bg.png";
 link3.href = "target.png";
 //tijd standaard uit
-timerAan = false;
+timerOn = false;
 
 //load als pagina geladen is
 function loadGame() {
     //alle componenten aanmaken
-    myScore = new component("score", "30px", "Consolas", "black", 0, 40, "text");
-    mytimer = new component("timer", "30px", "Consolas", "black", 800, 40, "text");
+    lblScore = new component("score", "30px", "Consolas", "black", 0, 40, "text");
+    lblSecondsPast = new component("timer", "30px", "Consolas", "black", 800, 40, "text");
     target = new component("target", 150, 50, link3.href, 600, 410, "image");
-    targetdetection = new component("target", 50, 2, "red", 650, 433);
-    targetdetection2 = new component("target", 25, 2, "white", 625, 433);
-    targetdetection3 = new component("target", 25, 2, "white", 700, 433);
-    targetdetection4 = new component("target", 25, 2, "red", 600, 433);
-    targetdetection5 = new component("target", 25, 2, "red", 725, 433);
-    progressbarbackside = new component("progressbar", 500, 20, "white", 200, 25);
-    progressbar = new component("progressbar", 500, 18, "red", 200, 26);
-    endText = new component("endText", "30px", "Consolas", "black", 100, 100, "text");
+    targetDetection = new component("target", 50, 2, "red", 650, 433);
+    targetDetection2 = new component("target", 25, 2, "white", 625, 433);
+    targetDetection3 = new component("target", 25, 2, "white", 700, 433);
+    targetDetection4 = new component("target", 25, 2, "red", 600, 433);
+    targetDetection5 = new component("target", 25, 2, "red", 725, 433);
+    progressbarBackground = new component("progressbar", 500, 20, "white", 200, 25);
+    progressbarHealth = new component("progressbar", 500, 18, "red", 200, 26);
+    endOfGameMessage = new component("endOfGameMessage", "30px", "Consolas", "black", 100, 100, "text");
     myBackground = new component("background", 1024, 435, link2.href, 0, 0, "image");
     duck = new component("duck", 50, 50, link1.href, 75, 195, "image");
-    duckhitbox = new component("duck", 1, 1, "black", 100, 245);          //hitbox en duck zijn 2 componenten maar alle movement is 2 keer
-    time = 0;               //aantal frames op 0 zetten
-    time2 = 0;              //tijd in seconden op 0 zetten
+    duckHitbox = new component("duck", 1, 1, "black", 100, 245);          //hitbox en duck zijn 2 componenten maar alle movement is 2 keer
+    frames = 0;               //aantal frames op 0 zetten
+    secondsPast = 0;              //tijd in seconden op 0 zetten
     score = 500; 
     amountsended = 0; 
     myGameArea.load();      //laad de canvas in
@@ -38,33 +39,41 @@ function loadGame() {
 //buttons
 function start() {
     //tijd aanleggen
-    timerAan = true;
+    if(secondsPast == 0){ //timer kan niet aan worden gelegd als die al aan staat (vermijd meermaals schieten)
+        timerOn = true;
+        canShoot = true;
+    }
 }
 
-function sendit() {
+function shoot() {
     //ophalen van snelheid (slider ingesteld in html: 1-6)
-    var speed = document.getElementById("speedx").value;
-    console.log(parseFloat(speed, 10));
-    testscore = score;                              //testscore gelijkstellen zodat de score niet blijft -100 ofzo doen als de hitbox de detection raakt
-    duck.gravity = 0.05;                            //zwaartekracht aanmaken zodat de eend valt
-    duckhitbox.gravity = 0.05;                      
-    duck.speedX = parseFloat(speed, 10);            //horizontale snelheid volgens de slider waarde
-    duckhitbox.speedX = parseFloat(speed, 10);
-    duck.speedY = -2;                               //verticale snelheid zodat de eend eerst beetje omhoog gaat (meer parabool vorm dan gwn vallen)
-    duckhitbox.speedY = -2;
-}
-
-function reshoot() {
-    //locatie eend resetten
-    duck = new component("duck", 50, 50, link1.href, 75, 195, "image");
-    duckhitbox = new component("duck", 1, 1, "black", 100, 245);
+    if(canShoot == true){
+        canShoot = false;
+        var speed = document.getElementById("speedx").value;
+        console.log(parseFloat(speed, 10));
+        checkScore = score;                              //checkScore gelijkstellen zodat de score niet blijft -100 ofzo doen als de hitbox de detection raakt
+        duck.gravity = 0.05;                            //zwaartekracht aanmaken zodat de eend valt
+        duckHitbox.gravity = 0.05;                      
+        duck.speedX = parseFloat(speed, 10);            //horizontale snelheid volgens de slider waarde
+        duckHitbox.speedX = parseFloat(speed, 10);
+        duck.speedY = -2;                               //verticale snelheid zodat de eend eerst beetje omhoog gaat (meer parabool vorm dan gwn vallen)
+        duckHitbox.speedY = -2;
+    }
 }
 
 function reload() {
-    timerAan = false;       //tijd terug uit zetten
+    //locatie eend resetten
+    canShoot = true;
+    duck = new component("duck", 50, 50, link1.href, 75, 195, "image");
+    duckHitbox = new component("duck", 1, 1, "black", 100, 245);
+}
+
+/*I suppose you can remove this*/
+/*function refresh() {
+    timerOn = false;       //tijd terug uit zetten
     myGameArea.stop();      //canvas freezen
     loadGame();             //volledige game terug aanmaken
-}
+}*/
 
 //gameaerea aka canvas
 var myGameArea = {
@@ -168,62 +177,64 @@ function updateGameArea() {
 
     //score controleren op einde spel
     if (score <= 0){
-        endText.text = "Gewonnen :)";
-        timerAan = false;
+        score = 0;
+        progressbarHealth.width = 0;
+        endOfGameMessage.text = "Gewonnen :)";
+        timerOn = false;
         myGameArea.stop();
     }
 
-    //checken of de hitbox de targetdetection raakt
-    if (duckhitbox.crashWith(targetdetection4) || duckhitbox.crashWith(targetdetection5)) {     //buitenste ring 25 punten
-        if (testscore == score){
+    //checken of de hitbox de targetDetection raakt
+    if (duckHitbox.crashWith(targetDetection4) || duckHitbox.crashWith(targetDetection5)) {     //buitenste ring 25 punten
+        if (checkScore == score){
             score -= 25;
-            progressbar.width -= 25;
+            progressbarHealth.width -= 25;
         }
     }
-    if (duckhitbox.crashWith(targetdetection2) || duckhitbox.crashWith(targetdetection3)) {     //middelste ring 50 punten
-        if (testscore == score){
+    if (duckHitbox.crashWith(targetDetection2) || duckHitbox.crashWith(targetDetection3)) {     //middelste ring 50 punten
+        if (checkScore == score){
             score -= 50;
-            progressbar.width -= 50;
+            progressbarHealth.width -= 50;
         }
     }
-    if (duckhitbox.crashWith(targetdetection)) {        //middelpunt 100 punten
-        if (testscore == score){
+    if (duckHitbox.crashWith(targetDetection)) {        //middelpunt 100 punten
+        if (checkScore == score){
             score -= 100;
-            progressbar.width -= 100;
+            progressbarHealth.width -= 100;
         }
     }
 
     //tijd aanpassen
-    if (timerAan == true){
-        time += 1;              //aantal frames berekenen
-        if (time == 50){        //game doet 50 frames per seconde
-            time2 += 1;         //aantal seconden berekenen
-            time = 0;
+    if (timerOn == true){
+        frames += 1;              //aantal frames berekenen
+        if (frames == 50){        //game doet 50 frames per seconde
+            secondsPast += 1;         //aantal seconden berekenen
+            frames = 0;
         }
     }
     
     myGameArea.clear();     //canvas clearen
 
-    duckhitbox.newPos();    //nieuwe positie van duck instellen
+    duckHitbox.newPos();    //nieuwe positie van duck instellen
     duck.newPos();
 
-    mytimer.text = "tijd: " + time2;    //text aanpassen van tijd en score
-    myScore.text = "SCORE: " + score;
+    lblSecondsPast.text = "tijd: " + secondsPast;    //text aanpassen van tijd en score
+    lblScore.text = "SCORE: " + score;
 
     //deze orde bepaalt de stacking order: meer naar onder komt het voorandere componenten te staan
     //alles updaten: terug visueel maken na clearen
-    duckhitbox.update();
-    targetdetection.update();
-    targetdetection2.update();
-    targetdetection3.update();
-    targetdetection4.update();
-    targetdetection5.update();
+    duckHitbox.update();
+    targetDetection.update();
+    targetDetection2.update();
+    targetDetection3.update();
+    targetDetection4.update();
+    targetDetection5.update();
     myBackground.update();
     target.update();
     duck.update();
-    myScore.update();
-    mytimer.update();
-    progressbarbackside.update();
-    progressbar.update();
-    endText.update();
+    lblScore.update();
+    lblSecondsPast.update();
+    progressbarBackground.update();
+    progressbarHealth.update();
+    endOfGameMessage.update();
 }
