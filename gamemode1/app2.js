540a34a4df24eebc85cc72857b8c4a469ec17d6e
HannesD <hannes.deblaere@student.howest.be>
2021-01-05 16:06:42 +0100

    added comments1

--- gamemode1/app2.js
+++ gamemode1/app2.js
@@ -1,16 +1,19 @@
+//variablen definieren
 var duck, duckhitbox, target, targetdetection, targetdetection2, targetdetection3, targetdetection4, targetdetection5, progressbarbackside, progressbar;
 var myScore, score, testscore, endText, timerAan, mytimer, time, time2;
+//img ophalen
 var link1 = document.createElement('link');
 var link2 = document.createElement('link');
 var link3 = document.createElement('link');
-//var link3 = document.createElement('link');
 link1.href = "quak.png";
 link2.href = "bg.png";
 link3.href = "target.png";
+//tijd standaard uit
 timerAan = false;
 
-//load
+//load als pagina geladen is
 function loadGame() {
+    //alle componenten aanmaken
     myScore = new component("score", "30px", "Consolas", "black", 0, 40, "text");
     mytimer = new component("timer", "30px", "Consolas", "black", 800, 40, "text");
     target = new component("target", 150, 50, link3.href, 600, 410, "image");
@@ -22,44 +25,45 @@ function loadGame() {
     progressbarbackside = new component("progressbar", 500, 20, "white", 200, 25);
     progressbar = new component("progressbar", 500, 18, "red", 200, 26);
     endText = new component("endText", "30px", "Consolas", "black", 100, 100, "text");
-    //console.log(endScore);
     myBackground = new component("background", 1024, 435, link2.href, 0, 0, "image");
     duck = new component("duck", 50, 50, link1.href, 75, 195, "image");
-    duckhitbox = new component("duck", 1, 1, "black", 100, 245);
-    time = 0;
-    time2 = 0;
-    score = 500;
-    amountsended = 0;
-    amountsendedafter = 1;
-    myGameArea.load();
+    duckhitbox = new component("duck", 1, 1, "black", 100, 245);          //hitbox en duck zijn 2 componenten maar alle movement is 2 keer
+    time = 0;               //aantal frames op 0 zetten
+    time2 = 0;              //tijd in seconden op 0 zetten
+    score = 500; 
+    amountsended = 0; 
+    myGameArea.load();      //laad de canvas in
 }
 
 //buttons
 function start() {
+    //tijd aanleggen
     timerAan = true;
 }
 
 function sendit() {
+    //ophalen van snelheid (slider ingesteld in html: 1-6)
     var speed = document.getElementById("speedx").value;
     console.log(parseFloat(speed, 10));
-    testscore = score;
-    duck.gravity = 0.05;
-    duckhitbox.gravity = 0.05;
-    duck.speedX = parseFloat(speed, 10);
+    testscore = score;                              //testscore gelijkstellen zodat de score niet blijft -100 ofzo doen als de hitbox de detection raakt
+    duck.gravity = 0.05;                            //zwaartekracht aanmaken zodat de eend valt
+    duckhitbox.gravity = 0.05;                      
+    duck.speedX = parseFloat(speed, 10);            //horizontale snelheid volgens de slider waarde
     duckhitbox.speedX = parseFloat(speed, 10);
-    duck.speedY = -2;
+    duck.speedY = -2;                               //verticale snelheid zodat de eend eerst beetje omhoog gaat (meer parabool vorm dan gwn vallen)
     duckhitbox.speedY = -2;
 }
 
 function reshoot() {
+    //locatie eend resetten
     duck = new component("duck", 50, 50, link1.href, 75, 195, "image");
     duckhitbox = new component("duck", 1, 1, "black", 100, 245);
 }
 
 function reload() {
-    timerAan = false;
-    myGameArea.stop();
-    loadGame();
+    timerAan = false;       //tijd terug uit zetten
+    myGameArea.stop();      //canvas freezen
+    loadGame();             //volledige game terug aanmaken
 }
 
 //gameaerea aka canvas
@@ -161,53 +165,62 @@ function component(name, width, height, color, x, y, type) {
 
 //updategame (gebeurt 50 keer per seconde)
 function updateGameArea() {
+
+    //score controleren op einde spel
     if (score <= 0){
         endText.text = "Gewonnen :)";
         timerAan = false;
         myGameArea.stop();
     }
-    if (duckhitbox.crashWith(targetdetection4) || duckhitbox.crashWith(targetdetection5)) {
+
+    //checken of de hitbox de targetdetection raakt
+    if (duckhitbox.crashWith(targetdetection4) || duckhitbox.crashWith(targetdetection5)) {     //buitenste ring 25 punten
         if (testscore == score){
             score -= 25;
             progressbar.width -= 25;
         }
     }
-    if (duckhitbox.crashWith(targetdetection2) || duckhitbox.crashWith(targetdetection3)) {
+    if (duckhitbox.crashWith(targetdetection2) || duckhitbox.crashWith(targetdetection3)) {     //middelste ring 50 punten
         if (testscore == score){
             score -= 50;
             progressbar.width -= 50;
         }
     }
-    if (duckhitbox.crashWith(targetdetection)) {
+    if (duckhitbox.crashWith(targetdetection)) {        //middelpunt 100 punten
         if (testscore == score){
             score -= 100;
             progressbar.width -= 100;
         }
     }
+
+    //tijd aanpassen
     if (timerAan == true){
-        time += 1;
-        if (time == 50){
-            time2 += 1;
+        time += 1;              //aantal frames berekenen
+        if (time == 50){        //game doet 50 frames per seconde
+            time2 += 1;         //aantal seconden berekenen
             time = 0;
         }
     }
     
-    myGameArea.clear();
+    myGameArea.clear();     //canvas clearen
 
-    duckhitbox.newPos();
+    duckhitbox.newPos();    //nieuwe positie van duck instellen
     duck.newPos();
-    mytimer.text = "tijd: " + time2;
+
+    mytimer.text = "tijd: " + time2;    //text aanpassen van tijd en score
     myScore.text = "SCORE: " + score;
-    myBackground.update();
-    target.update();
+
+    //deze orde bepaalt de stacking order: meer naar onder komt het voorandere componenten te staan
+    //alles updaten: terug visueel maken na clearen
+    duckhitbox.update();
     targetdetection.update();
     targetdetection2.update();
     targetdetection3.update();
     targetdetection4.update();
     targetdetection5.update();
+    myBackground.update();
+    target.update();
     duck.update();
-    duckhitbox.update();
-    
     myScore.update();
     mytimer.update();
     progressbarbackside.update();
