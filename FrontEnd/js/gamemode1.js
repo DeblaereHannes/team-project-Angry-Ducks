7e633aa0ca08d2fbddca913895c86b0bf73045ac
HannesD <hannes.deblaere@student.howest.be>
2021-01-09 11:16:25 +0100

    ENH GameAreaClass made

--- FrontEnd/js/gamemode1.js
+++ FrontEnd/js/gamemode1.js
@@ -87,44 +87,6 @@ const reload = function() {
 
 }
 
-//gameaerea aka canvas
-var myGameArea = {
-    canvas: document.createElement("canvas"),
-    //canvas inladen
-    load: function() {
-        this.canvas.width = viewport;
-        this.canvas.height = (viewport * 0.4248046875);
-        this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.interval = setInterval(updateGameArea, 20);
-        window.addEventListener('mousedown', function (e) {
-            myGameArea.x = e.pageX;
-            myGameArea.y = e.pageY;
-        })
-        window.addEventListener('mouseup', function (e) {
-            myGameArea.x = false;
-            myGameArea.y = false;
-        })
-        window.addEventListener('touchstart', function (e) {
-            myGameArea.x = e.pageX;
-            myGameArea.y = e.pageY;
-        })
-        window.addEventListener('touchend', function (e) {
-            myGameArea.x = false;
-            myGameArea.y = false;
-        })
-    },
-    //canvas clearen (alles wissen zodat je kan "hertekenen")
-    clear: function() {
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    },
-    //canvas freezen
-    stop: function() {
-        clearInterval(this.interval);
-    }
-}
-
-
 
 //updategame (gebeurt 50 keer per seconde)
 const updateGameArea = function() {
