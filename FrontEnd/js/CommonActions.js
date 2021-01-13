183f0fddd4a660bf166354e1f6303c277d3da2de
HannesD <hannes.deblaere@student.howest.be>
2021-01-13 17:25:17 +0100

    MIN put functions back in 1 js en used regions

--- FrontEnd/js/CommonActions.js
+++ FrontEnd/js/CommonActions.js
@@ -1,3 +1,8 @@
+//#region ***  Variablen ***
+
+let chosenHeartRateService = null;
+var HR;
+var showPauseMenu = false, btnPause,btnExit;
 var canShoot, CalmHR, ShootHR,duckPlayer1 = 0; duckPlayer2 = 3;
 var characters = ["", "", "","", "", "","", "", ""]
 for(link of characters)
@@ -13,7 +18,11 @@ characters[5] = "./img/characters/Duck_Female3.png";
 characters[6] = "./img/characters/Duck_White.png";
 characters[7] = "./img/characters/Duck_Yellow.png";
 characters[8] = "./img/characters/Duck_Gray.png";
-//buttons
+
+//#endregion
+
+//#region *** Start Function ***
+
 const start = function() {
     //tijd aanleggen
     if (secondsPast == 0 && showPauseMenu == false) { //timer kan niet aan worden gelegd als die al aan staat (vermijd meermaals schieten)
@@ -21,6 +30,10 @@ const start = function() {
         canShoot = true;
     }
 }
+
+//#endregion
+
+//#region *** Shoot Function ***
 
 const shoot = function() {
     //ophalen van snelheid (slider ingesteld in html: 1-6)
@@ -46,6 +59,10 @@ const shoot = function() {
     }
 }
 
+//#endregion
+
+//#region *** Reload 1 duck Function ***
+
 const reload = function() {
     //locatie eend resetten
     if(secondsPast != 0 && showPauseMenu == false){
@@ -53,8 +70,12 @@ const reload = function() {
         duck = new component("duck", (viewport * 0.048828125), (viewport * 0.048828125), links[0], (viewport * 0.0732421875), (viewport * 0.1904296875), "image");
         duckHitbox = new component("duckhitbox", 1, 1, "black", (viewport * 0.09765625), (viewport * 0.238));
     }
-
 }
+
+//#endregion 
+
+//#region *** restart game function ***
+
 const refresh = function() {
     timerOn = false;       //tijd terug uit zetten
     canShoot = false;
@@ -63,11 +84,18 @@ const refresh = function() {
     loadGame();             //volledige game terug aanmaken
 }
 
+//#endregion
+
+//#region *** set rusthartslag function ***
+
 const rusthartslag = function() {
     CalmHR = HR;
     document.querySelector('.js-rusthartslag').innerHTML = `rusthartslag: ${CalmHR}`;
 }
 
+//#endregion
+
+//#region *** characterSelection function ***
 
 const characterSelection = function(Number){
     switch(Number)
@@ -82,5 +110,103 @@ const characterSelection = function(Number){
     document.getElementById("0").src= characters[duckPlayer1];
     document.getElementById("1").src= characters[duckPlayer1];
     document.getElementById("2").src= characters[duckPlayer2];
-
 }
+
+//#endregion
+
+//#region *** pause menu functions ***
+
+const listenToButtons = function(){
+    btnExit.addEventListener("click", function(){
+        console.log("Exit Clicked");
+        hidePauseMenu();
+    });
+    btnPause.addEventListener("click", function(){
+        console.log("Pause Clicked");
+        showPauseMenu = true;
+        document.querySelector(".bgGamemode").classList.add("bgGamemode--blur");
+    });
+}
+
+const init = function() {
+    btnPause = document.querySelector(".js-pause");
+    btnExit = document.querySelector(".js-exit");
+    listenToButtons();
+}
+const hidePauseMenu = function(){
+    showPauseMenu = false; 
+    document.querySelector(".bgGamemode").classList.remove("bgGamemode--blur");
+}
+
+document.addEventListener("DOMContentLoaded", init);
+
+//#endregion
+
+//#region *** BT connection functions ***
+
+const BTconnection = function() {
+    //opent de bluetooth interface van google waar je aparaten kan koppelen
+    navigator.bluetooth.requestDevice({
+        filters: [{
+          services: ['heart_rate'],
+        }]                                                      //vanaf hier paar instellingen die ik niet versta
+      }).then(device => device.gatt.connect())                  
+      .then(server => server.getPrimaryService('heart_rate'))
+      .then(service => {
+        chosenHeartRateService = service;
+        return Promise.all([
+          service.getCharacteristic('heart_rate_measurement')
+            .then(handleHeartRateMeasurementCharacteristic),
+        ]);
+      });
+  };
+
+function handleHeartRateMeasurementCharacteristic(characteristic) {
+    console.log(characteristic);
+  return characteristic.startNotifications()
+  .then(char => {
+    characteristic.addEventListener('characteristicvaluechanged', onHeartRateChanged);
+  });
+}
+
+function onHeartRateChanged(event) {
+  const characteristic = event.target;
+  //console.log(parseHeartRate(characteristic.value));
+  HR = parseHeartRate(characteristic.value).heartRate;
+  document.querySelector('.js-liveHR').innerHTML = `live heart rate: ${HR}`;
+}
+
+function parseHeartRate(data) {         //functie die de heartrate leesbaar maakt
+    const flags = data.getUint8(0);
+    const rate16Bits = flags & 0x1;
+    const result = {};
+    let index = 1;
+    if (rate16Bits) {
+      result.heartRate = data.getUint16(index, /*littleEndian=*/true);
+      index += 2;
+    } else {
+      result.heartRate = data.getUint8(index);
+      index += 1;
+    }
+    const contactDetected = flags & 0x2;
+    const contactSensorPresent = flags & 0x4;
+    if (contactSensorPresent) {
+      result.contactDetected = !!contactDetected;
+    }
+    const energyPresent = flags & 0x8;
+    if (energyPresent) {
+      result.energyExpended = data.getUint16(index, /*littleEndian=*/true);
+      index += 2;
+    }
+    const rrIntervalPresent = flags & 0x10;
+    if (rrIntervalPresent) {
+      const rrIntervals = [];
+      for (; index + 1 < data.byteLength; index += 2) {
+        rrIntervals.push(data.getUint16(index, /*littleEndian=*/true));
+      }
+      result.rrIntervals = rrIntervals;
+    }
+    return result;
+  }
+
+//#endregion
